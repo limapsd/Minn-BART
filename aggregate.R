985b74ab734c72b286dd56e21363db8e41226905
limapsd <limapedroasb@gmail.com>
2025-01-30 21:18:40 -0600

    cluster commands and scripts

---
+++ aggregate.R
@@ -0,0 +1,45 @@
+load("MacroUncQ.rda")
+
+lags <- 13
+M  <- length(var.sets$`VAR-22`)
+K  <- M*lags
+
+T0 <- 160
+T1 <- nrow(yraw)
+h  <- 4
+estimation_window <-  T1-T0-h
+
+x_names <- paste0(var.sets$`VAR-22`, ".t-", sort(rep(1:lags,M)))
+
+model_names <- c("bart", "dart", "minn", "bartfsv", "dartfsv", "minnfsv")
+num_models <- length(model_name)
+
+point_forecasts_array <- array(data = NA, c(estimation_window, h, M, num_models))
+dimnames(point_forecasts_array) <- list(paste0("w_",1:estimation_window), paste0("t+",1:h), var.sets$`VAR-22`, model_names)
+
+var_count_array <- array(data = NA, c(estimation_window, K, M, num_models))
+dimnames(var_count_array) <- list(paste0("w_",1:estimation_window), x_names, var.sets$`VAR-22`,model_names)
+
+volatilty_forecasts_array <- array(data = NA, c(estimation_window, h, M, num_models))
+dimnames(volatilty_forecasts_array) <- list(paste0("w_",1:estimation_window), paste0("t+",1:h), var.sets$`VAR-22`, model_names)
+
+for(model in model_name){
+  for(n_w in 1:estimation_window){
+    
+    files <- list.files("/Results")
+    selected_file <- files[grep(paste0(model,"_window_", n_w, "\\.rds$"), files)]
+    saved_model <- readRDS(paste0("/Results/",selected_file))
+    
+    predicitons <- saved_model$predictions
+    volatility  <- saved_model$sigma_predictions
+    var_count   <- saved_model$var_count_matrix
+    
+    point_forecasts_array[n_w,,,model]     <- apply(predicitons, 2:3, median)
+    var_count_array[n_w,,,model]           <- apply(var_count>0, 2:3, mean)
+    volatilty_forecasts_array[n_w,,,model] <- apply(volatility, 2:3, median)
+  }
+}
+
+aggregation <- list("forecast" = point_forecasts_array,"vol_forecast" = volatilty_forecasts_array, "var_count" = var_count_array)
+saveRDS(aggregation,file = paste0("/Aggregation/"))
+
---
+++ bart_est.R
@@ -0,0 +1,31 @@
+load(paste0("/MacroUncQ.rda"))
+
+library(stochtree)
+
+yraw_f <- yraw[,var.sets$`VAR-22`]
+time_index <- time(yraw)
+rownames(yraw_f) <-time_index
+# Training Data from 1965Q1 - 2004Q4 1-160
+
+T0 <- 160
+T1 <- nrow(yraw_f)
+h  <- 4
+
+num_burnin <-20000
+num_mcmc   <-10000
+
+for(i in 1:(T1-T0-h)){
+  
+  y_train <- yraw_f[1:(T0 + i -1),]
+  y_test  <- yraw_f[(T0 + i):(T0 + i + h -1),]
+  
+  print(paste0("Expanding Window... ", i))
+  
+  model <- stochtree::fsv_mbart(data = y_train, Y_test = y_test, n_ahead = h, lags = 13,
+                                num_burnin = num_burnin, num_mcmc = num_mcmc)
+  
+  saveRDS(model, file = paste0("/Results/bart_window_",i,".rds"))
+  rm(model)
+  gc()
+}
+
