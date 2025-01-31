75d208c063c63b9da6e015ecc8275288ce50d8ed
limapsd <limapedroasb@gmail.com>
2025-01-30 21:37:51 -0600

    final batch commands

--- main.R
+++ main.R
@@ -1,3 +1,15 @@
 
+model_names <- c("bart", "dart", "minn", "bartfsv", "dartfsv", "minnfsv")
+n_models <-length(model_names)
 
-list.files
+if("Results" %in% list.files() == FALSE) dir.create("Results")
+if("Agreggation" %in% list.files() == FALSE) dir.create("Agreggation")
+if("Logs" %in% list.files() == FALSE) dir.create("Logs")
+
+for(i in 1:n_models){
+    scripts_name <- paste0(model_names[i],"_est.R")   
+    batch.script <- paste0("nice Rscript --verbose ",scripts_name," 2>&1 Logs/")
+
+    system(batch.script)
+    Sys.sleep(5)
+}
