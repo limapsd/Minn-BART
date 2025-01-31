7f5fa519124b9e9bd5ca48cd47f8d9100b152679
limapsd <limapedroasb@gmail.com>
2025-01-31 02:11:02 -0600

    error in main_ind

--- main_ind.R
+++ main_ind.R
@@ -2,20 +2,20 @@ main <- function(){
     args <- commandArgs(TRUE)
     
     # Ensure directories exist
-    if(!"Results" %in% list.files()) dir.create("Results")
-    if(!"Aggregation" %in% list.files()) dir.create("Aggregation")
-    if(!"Logs" %in% list.files()) dir.create("Logs")
+    if (!"Results" %in% list.files()) dir.create("Results")
+    if (!"Aggregation" %in% list.files()) dir.create("Aggregation")
+    if (!"Logs" %in% list.files()) dir.create("Logs")
 
     model_names <- c("bart", "dart", "minn", "bart_fsv", "dart_fsv", "minn_fsv")
     
     # Handle missing argument case
-    if(length(args) < 1) {
-        stop("Error: No model name provided. Please specify a valid model. Choose from:", paste(model_names, collapse=", ")
+    if (length(args) < 1) {
+        stop("Error: No model name provided. Please specify a valid model.")
     }
 
     model <- as.character(args[1])
 
-    if(model %in% model_names){
+    if (model %in% model_names) {
         scripts_name <- paste0(model, "_est.R")   
         batch.script <- paste0("nice Rscript --verbose ", scripts_name, " 2>&1 Logs/", model, ".txt &")
         system(batch.script)
